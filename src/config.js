377e2f379b3215b7d9b86d47418053c230cea9c9
saturnalia <comiller76@hotmail.com>
2020-02-23 19:45:24 -0800

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads1991"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2x3s6gfwsf.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_x4W6mvRvw",
+    APP_CLIENT_ID: "62ka5qf8lj6csb0636icp0gd39",
+    IDENTITY_POOL_ID: "us-east-1:4fbd54f5-9172-4587-a680-9e87bd2fd10d"
+  }
+};
